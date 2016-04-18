bdd0788b8044660d4e206c38fcb434fed8985831
chabbaboy <chabba@gmail.com>
2016-04-18 10:01:27 +0200

    update routes - merge import into app

---
+++ src/helpers/functions.ts
@@ -0,0 +1,72 @@
+import {bimObject} from "./bimObjectsData";
+var uuid = require('uuid');
+import {roomCategoryData} from "./roomCategoryData";
+import {roomInstancesData} from "./roomInstancesData";
+import {assetGeometriesData} from "./assetGeometriesData";
+import {assetMaterijalsData} from "./assetMaterijalsData";
+import {assetObjectsData} from "./assetObjectsData";
+
+
+export function getRandomBimObject():{} {
+
+    let result = {};
+
+
+    const bimRandomObject  = bimObject[Math.floor(Math.random() * bimObject.length)];
+    result["complex"]  = bimRandomObject.complex;
+
+    const bimRandomEntity = bimRandomObject.entity[Math.floor(Math.random() * bimRandomObject.entity.length)];
+    result["entity"]= bimRandomEntity.entity;
+
+    const bimRandomModel = bimRandomEntity.model[Math.floor(Math.random() * bimRandomEntity.model.length)];
+    result["model"] = bimRandomModel.model;
+
+    const bimRandomLevel = bimRandomModel.level[Math.floor(Math.random() * bimRandomModel.level.length)];
+    result["level"] = bimRandomLevel;
+
+    return result;
+
+}
+
+export function getRandomEntireBimObject ():{} {
+
+    let bimObject = getRandomBimObject();
+
+    bimObject["guid"] = uuid.v4();
+    bimObject["category"] = roomCategoryData [Math.floor(Math.random() * roomCategoryData.length)].name;  //room category
+    bimObject["group"]  = "grupa"; // grupa
+    bimObject["space"]  = "Space";  // nista
+    bimObject["data"] =  getRandomReformatRoomInstanceData();  //roomintsance
+
+    bimObject["checkouts"]  = {"projects": []};
+    
+
+    bimObject["geometry"]  = assetGeometriesData[Math.floor(Math.random() * assetGeometriesData.length)];
+    bimObject["material"]  = assetMaterijalsData[Math.floor(Math.random() * assetMaterijalsData.length)];
+    bimObject["object"]  = assetObjectsData[Math.floor(Math.random() * assetObjectsData.length)];
+    
+    return bimObject;
+    
+}
+
+export function getRandomReformatRoomInstanceData (){
+
+    //take random element from room instances array
+    const lvRoomInstances : Number = Math.floor(Math.random() * roomInstancesData.length);
+    const lvObject = roomInstancesData[lvRoomInstances];
+
+    const newRoomInstance=[];
+
+        //transform existing object   from : propriety_name  value  >> to : key: propriety_name   value: value
+        Object.keys(lvObject).forEach(function(key,index) {
+
+            let lvObj = new Object;
+            lvObj.key=""+key+"";
+            lvObj.value = lvObject[""+key+""];
+            newRoomInstance[index]=lvObj;
+        });
+
+    return newRoomInstance;
+
+}
+
---
+++ src/helpers/import.ts
@@ -0,0 +1,20 @@
+import {BimModel} from "../model/bim/model";
+import { getRandomEntireBimObject}from "./functions";
+
+
+export function insertBimObjects():void {
+    // Array contains Object combinations for complex,entity,model,level
+
+    const bimValues = new Array;
+
+    for (let i = 0; i <100000; i++) {
+
+        const lvBimObject = getRandomEntireBimObject();
+
+        bimValues.push(lvBimObject);
+    }
+    console.log("insert bimObjects starts");
+    BimModel.collection.insert(bimValues);
+
+
+}
